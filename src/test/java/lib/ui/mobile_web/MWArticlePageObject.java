ad40fc4c7bb754209fc312bb4121b307f960ad7d
Svetlana <ma1m2@mail.ru>
2019-12-04 11:34:24 +0400

    8_lesson_HW_Ex17
    MyListsTests     testSaveTwoArticleIntoOneFolder()

--- src/test/java/lib/ui/mobile_web/MWArticlePageObject.java
+++ src/test/java/lib/ui/mobile_web/MWArticlePageObject.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 public class MWArticlePageObject extends ArticlePageObject {
   static {
     TITLE = "css:#content h1";//Java (programming language)
-    TITLE_2 = "css:.page-summary:nth-child(1)";//Java
+    TITLE_2 = "css:#content h1";//Java
     LINK_INSTEAD_OF_TITLE_2 = "css:#mf-section-0 > p:nth-child(2) > a:nth-child(2)";
     FOOTER_ELEMENT = "css:footer";
     OPTION_REMOVE_FROM_MY_LIST_BUTTON = "xpath://a[contains(text(),'Unwatch')]";//#page-actions li#ca-watch.mw-ui-icon-mf-watched watched button
--- src/test/java/tests/MyListsTests.java
+++ src/test/java/tests/MyListsTests.java
@@ -10,7 +10,8 @@ import lib.ui.factories.SearchPageObjectFactory;
 import org.junit.Test;
 
 public class MyListsTests extends CoreTestCase {
-  private static final String NAME_OF_FOLDER = "Learning programming";
+  private static final String NAME_OF_FOLDER = "ReadLater";
+  private static final String SEARCH_LINE = "Java";
   private static final String LOGIN = "Sveta 2607";
   private static final String PASSWORD = "W1234asdf#";
 
@@ -19,7 +20,7 @@ public class MyListsTests extends CoreTestCase {
     SearchPageObject searchPageObject = SearchPageObjectFactory.get(driver);
 
     searchPageObject.initSearchInput();
-    searchPageObject.typeSearchLine("Java");
+    searchPageObject.typeSearchLine(SEARCH_LINE);
     searchPageObject.clickArticleBySubstring("bject-oriented programming language");
 
     ArticlePageObject articlePageObject = ArticlePageObjectFactory.get(driver);
@@ -63,34 +64,44 @@ public class MyListsTests extends CoreTestCase {
     System.out.println("Well done! The testSaveArticleToMyList has been passed successfully!");
   }
 
-  @Test//6_lesson_HW_Ex11_corrected
+  @Test//8_lesson_HW_Ex17
   public void testSaveTwoArticleIntoOneFolder() {
     SearchPageObject searchPageObject = SearchPageObjectFactory.get(driver);
     searchPageObject.initSearchInput();
-    searchPageObject.typeSearchLine("Java");
+    searchPageObject.typeSearchLine(SEARCH_LINE);
     searchPageObject.clickArticleBySubstring("bject-oriented programming language");
 
     ArticlePageObject articlePageObject = ArticlePageObjectFactory.get(driver);
     articlePageObject.waitForTitleElement();
-    String articleTitle1 = articlePageObject.getArticleTitle();
+    String articleTitle= articlePageObject.getArticleTitle();
 
     if (Platform.getInstance().isAndroid()) {
       articlePageObject.addArticleToMyList(NAME_OF_FOLDER);
     } else {
       articlePageObject.addArticleToMySaved();
     }
+    if (Platform.getInstance().isMW()) {
+      AuthorizationPageObject auth = new AuthorizationPageObject(driver);
+      auth.clickAuthButton();
+      auth.enterLoginData(LOGIN, PASSWORD);
+      auth.submitForm();
+
+      articlePageObject.waitForTitleElement();
+      assertEquals("We are not on the same page after login", articleTitle, articlePageObject.getArticleTitle());
+      articlePageObject.addArticleToMySaved();
+    }
     articlePageObject.closeArticle();
 
     //1.Save the second article into the same folder 'ReadLater'
     MyListPageObject myListPageObject = MyListPageObjectFactory.get(driver);
-    if (Platform.getInstance().isAndroid()) {
-      searchPageObject.initSearchInput();
-      searchPageObject.typeSearchLine("Java");
-    } else {
+    if (Platform.getInstance().isIOS()) {
       searchPageObject.repeatedSearchInput();
+    } else {
+      searchPageObject.initSearchInput();
+      searchPageObject.typeSearchLine(SEARCH_LINE);
     }
     searchPageObject.clickArticleBySubstring("Island of Indonesia");//Island of Indonesia
-    articlePageObject.waitForTitleSecondArticle();
+    articlePageObject.waitForTitleSecondArticle();//because on iOS TITLE differs TITLE_2
     if (Platform.getInstance().isAndroid()) {
       articlePageObject.addSecondArticleToMyList();
       myListPageObject.clickFolderByName(NAME_OF_FOLDER);
@@ -100,39 +111,28 @@ public class MyListsTests extends CoreTestCase {
     articlePageObject.closeArticle();
 
     NavigationUI navigationUI = NavigationUIFactory.get(driver);
+    navigationUI.openNavigation();
     navigationUI.clickMyLists();
     if (Platform.getInstance().isAndroid()) {
       myListPageObject.openFolderByName(NAME_OF_FOLDER); //work sometimes
       //myListPageOb.openFolderByImageID(); always work
     }
-
+    if (Platform.getInstance().isMW()) {
+      driver.navigate().refresh();
+    }
     //2.Delete the first article     //Java (programming language)
     if (Platform.getInstance().isIOS()) {
       myListPageObject.removeOverlay();
     }
-    myListPageObject.swipeArticleToDelete(articleTitle1);
+    myListPageObject.swipeArticleToDelete(articleTitle);
 
     //3.To be sure that the second article is present 4.Go into it
-    myListPageObject.openSecondArticle("Java");
+    myListPageObject.openSecondArticle(SEARCH_LINE);
     //4.Be sure that we have a special link inside an article
     articlePageObject.waitElementInsideArticle();
 
     System.out.println("Well done! The testSaveTwoArticleIntoOneFolder has been passed successfully!");
   }
 
-
-  @Test//Check link Indonesian
-  public void testSaveIndonesiaToMyList() {
-    SearchPageObject searchPageObject = SearchPageObjectFactory.get(driver);
-
-    searchPageObject.initSearchInput();
-    searchPageObject.typeSearchLine("Java");
-    searchPageObject.clickArticleBySubstring("Island of Indonesia");
-
-    ArticlePageObject articlePageObject = ArticlePageObjectFactory.get(driver);
-    articlePageObject.waitElementInsideArticle();
-
-    System.out.println("Well done! The testSaveIndonesiaToMyList has been passed successfully!");
-  }
 }
 
